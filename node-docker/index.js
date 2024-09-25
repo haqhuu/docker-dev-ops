fdbfae430254d9d8904f980dab874151e7eec7db
haquochuu <huu3675@gmail.com>
2024-09-25 19:04:37 +0700

    install mongoose, create config

--- node-docker/index.js
+++ node-docker/index.js
@@ -1,12 +1,23 @@
-const express = require('express')
 
+const express = require('express')
+const { MONGO_USER, MONGO_PASSWORD, MONGO_IP, MONGO_PORT } = require("./config/config")
 const app = express()
+
+const CONNECTION_STRING = `mongodb://${process.env.MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`
+
+const mongoose = require('mongoose')
+
+mongoose.connect(CONNECTION_STRING).then(
+    () => console.log("connected to server!")
+).catch(e => console.log(e))
+
 
 const port = process.env.PORT || 3000;
 
+
 app.get('/', (req, res) => {
 
-    res.send(`<h2>ghi </h2>`)
+    res.send(`<h2>hhhhhhhhh </h2>`)
 })
 
 app.listen(port, () => {
