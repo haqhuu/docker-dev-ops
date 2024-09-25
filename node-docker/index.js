54afdd734f30dbfd338aea522d32a7a2f2ed74c1
haquochuu <huu3675@gmail.com>
2024-09-25 09:49:11 +0700

    dev, production

--- node-docker/index.js
+++ node-docker/index.js
@@ -5,8 +5,8 @@ const app = express()
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    console.log(`Server home`)
-    res.send(`<h2>222 There </h2>`)
+
+    res.send(`<h2>ghi </h2>`)
 })
 
 app.listen(port, () => {
