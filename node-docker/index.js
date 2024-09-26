73046004c285d9cbb0e271167f5d5b53d997c29b
haquochuu <huu3675@gmail.com>
2024-09-26 15:33:09 +0700

    try catch conn and CRUD post

--- node-docker/index.js
+++ node-docker/index.js
@@ -1,24 +1,45 @@
 
 const express = require('express')
-const { MONGO_USER, MONGO_PASSWORD, MONGO_IP, MONGO_PORT } = require("./config/config")
+const {
+    MONGO_USER,
+    MONGO_PASSWORD,
+    MONGO_IP,
+    MONGO_PORT
+} = require("./config/config")
+
+const postRouter = require("./routes/postRoutes")
+
 const app = express()
 
-const CONNECTION_STRING = `mongodb://${process.env.MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`
-
+const CONNECTION_STRING =
+    `mongodb://${process.env.MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`
 const mongoose = require('mongoose')
 
-mongoose.connect(CONNECTION_STRING).then(
-    () => console.log("connected to server!")
-).catch(e => console.log(e))
-
+const connectWithRetry = () => {
+    mongoose.connect(CONNECTION_STRING, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }).then(
+        () => console.log("connected to server!")
+    ).catch(e => {
+        console.log(e)
+        setTimeout(connectWithRetry, 5000)
+    })
+}
 
 const port = process.env.PORT || 3000;
+
+connectWithRetry()
+app.use(express.json())
 
 
 app.get('/', (req, res) => {
 
-    res.send(`<h2>hhhhhhhhh </h2>`)
+    res.send(`<h2 style="color: red;">fhdhfdh </h2>`)
 })
+
+//localhost:3000/api/v1/post/
+app.use("/api/v1/posts", postRouter)
 
 app.listen(port, () => {
     console.log(`listening on port ${port}`)
